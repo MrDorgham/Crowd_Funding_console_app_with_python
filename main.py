10ff9e584f75663dc868ae03b0a47d2bd3372db7
Mohamed_Dorgham <dorgham.inbox@gmail.com>
2023-01-03 17:27:02 +0200

    Register function + regx and checks + simple db.txt

--- main.py
+++ main.py
@@ -1,6 +1,58 @@
+import re
+
+email_regx = re.compile(r'([A-Za-z0-9]+[.-_])*[A-Za-z0-9]+@[A-Za-z0-9-]+(\.[A-Z|a-z]{2,})+')
+phone_regx = re.compile(r'^(?:\+?01)?[09]\d{10,10}$')
+
+
+def email_check(msg):
+    email = input(msg)
+    if re.fullmatch(email_regx, email):
+        return email
+    else:
+        return email_check("Enter a Valid email: ")
+
+
+def phone_check(msg):
+    phone = input(msg)
+    if re.fullmatch(phone_regx, phone):
+        return phone
+    else:
+        return phone_check("Enter a Valid phone number: ")
+
+
+def name_check(msg):
+    name = input(msg)
+    if not any(char.isdigit() for char in name):
+        return name
+    else:
+        name_check("Enter a Valid Name: ")
+    return name
 
 
 def Register():
+
+    f_name = name_check("Enter Your First Name: ")
+    l_name = name_check("Enter Your Last Name: ")
+    email = email_check("Enter Your email: ")
+
+    password = input("Enter Your Password: ")
+    confirm_password = input("Enter Your Password Again: ")
+
+    while confirm_password != password:
+        print("Passwords match !")
+        password = input("Enter Your Password: ")
+        confirm_password = input("Please Confirm Your Password: ")
+
+    phone = phone_check("Enter Your phone number: ")
+
+    print("You Have Registered Successfully!")
+    print(f"Your data (Name:{f_name} {l_name} Mail:{email} Password:{password} Phone Number:{phone} ")
+
+    fileobject = open("data/db.txt", "a")
+    fileobject.writelines(f_name + ":" + l_name + ":" + email + ":" + password + ":" + phone + "\n")
+    fileobject.close()
+
+
     print("reg")
     pass
 def Login():
